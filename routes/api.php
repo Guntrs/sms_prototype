4d78814285879ce61da23701bfcb02ce56e6094e
Gunther Sunum <guntergarcia761@gmail.com>
2026-05-16 18:52:32 -0600

    configuracion autotenticacioin

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/clientes', [ClienteController::class, 'index']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::post('/clientes', [ClienteController::class, 'store']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/clientes', [ClienteController::class, 'index']);
+    Route::post('/clientes', [ClienteController::class, 'store']);
+    Route::get('/clientes/{id}', [ClienteController::class, 'show']);
+    Route::put('/clientes/{id}', [ClienteController::class, 'update']);
+    Route::delete('/clientes/{id}', [ClienteController::class, 'destroy']);
+});
 
-Route::get('/clientes/{id}', [ClienteController::class, 'show']);
-
-Route::put('/clientes/{id}', [ClienteController::class, 'update']);
-
-Route::delete('/clientes/{id}', [ClienteController::class, 'destroy']);
